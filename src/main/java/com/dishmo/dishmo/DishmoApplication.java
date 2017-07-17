edad0a0b589dcd3622d68966da57766081cf581d
Cindy Colucy <ckcolucy@gmail.com>
2017-07-17 09:54:42 -0400

    Dismo Initial Commit

---
+++ src/main/java/com/dishmo/dishmo/DishmoApplication.java
@@ -0,0 +1,12 @@
+package com.dishmo.dishmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishmoApplication.class, args);
+	}
+}

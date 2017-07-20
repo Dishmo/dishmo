c2ccbceff4eedd9bc0d54d0daea24b02aebd7a60
Michaelondon <michaelondon6@gmail.com>
2017-07-20 15:43:35 -0400

    restaurants.html update
    added images and formated restaurants.html

--- src/main/java/com/dishmo/dishmo/RestaurantPopulator.java
+++ src/main/java/com/dishmo/dishmo/RestaurantPopulator.java
@@ -32,25 +32,25 @@ public class RestaurantPopulator implements CommandLineRunner {
 		Restaurant dosaCorner = new Restaurant("Dosa Corner", "1077 Old Henderson Rd. Columbus, OH 43220",
 				"www.dosacornerrestaurant.com", "614-459-5515", "Mon 5-9pm, Tues-Sat 11am-9pm, Sun 12-8pm",
 				"facebook.com/dosa2go", "na", "na", "yelp.com/biz/dosa-corner-columbus", "$", indian,
-				"/images/mangolassi.jpg");
+				"/images/mangolassi200x133.png");
 
 		Restaurant blackDiamond = new Restaurant("Black Diamond Bar & Bistro", "141 N. 4th St. Columbus, OH 43215",
 				"www.blackdiamondbarbistro.com", "419-614-5555", "Mon-Thurs 11 am - 12 am, Fri-Sun 11 am - 12 am", "na",
-				"na", "na", "na", "$$", american, "/images/blackdiamond.jpg");
+				"na", "na", "na", "$$", american, "/images/blackdiamond200x133.png");
 
 		Restaurant luckyDragon = new Restaurant("Lucky Dragon", "2800 N. High St. Columbus, OH 43202",
 				"www.luckydragonchineserestaurant.com", "614-263-6899", "Tues-Sat 11 am - 10 pm, Sund 11:30 am - 10 pm",
 				"na", "na", "na", "www.yelp.com/biz/lucky-dragon-columbus-2", "$", chinese,
-				"/images/Cashew-Chicken.jpg");
+				"/images/Cashew-Chicken200x133.png");
 
 		Restaurant coopersHawk = new Restaurant("Cooper's Hawk", "4230 The Strand Columbus, OH 43219",
 				"www.coopershawkwinery.com", "614-428-6999", "Mon- Sat 11 am - 930 pm", "www.facebook.com/coopershawk",
 				"na", "na", "yelp.com/biz/coopers-hawk-winery-and-restaurants-arlington-heights-2", "$$", american,
-				"/images/coopers.jpg");
+				"/images/coopers200x133.png");
 
 		Restaurant qsBombBistro = new Restaurant("Q's Bomb A** Bistro", "3609 Trabue Rd. Columbus, OH 43228",
 				"www.qsbb.com", "614-868-5490", "Mon-Sun 11 am - 9:30 pm", "facebook.com/qsbb", "na", "na", "na", "$",
-				fusion, "/images/fusion.jpg");
+				fusion, "/images/fusion200x133.png");
 
 		restRepo.save(dosaCorner);
 		restRepo.save(blackDiamond);

62825972eaaaa16b734dc6118671be7dd5a5dfd2
Cindy Colucy <ckcolucy@gmail.com>
2017-07-21 15:09:14 -0400

    populated!

--- src/main/java/com/dishmo/dishmo/RestaurantPopulator.java
+++ src/main/java/com/dishmo/dishmo/RestaurantPopulator.java
@@ -64,8 +64,64 @@ public class RestaurantPopulator implements CommandLineRunner {
 		MenuItem baconCheeseFries = new MenuItem("Bacon Cheese Fries", blackDiamond,
 				"Crispy crinkle cut fries topped with melted cheddar cheese, bacon, green onions, freshly cracked pepper and a side of sour cream and vinegar.",
 				"n/a", "$6.50");
+		MenuItem allAmericanBurger = new MenuItem("All American Burger", blackDiamond,
+				"Quarter Pound Burger cooked perfectly to order with red onions, lettuce, tomato, pickle and Black Diamond sauce. Served on a fresh toasted sesame seed bun. Comes with a side of crinkle cut fries.",
+				"n/a", "$12.00");
+		MenuItem angusPrimeRibeye = new MenuItem("12 oz Angus Prime Ribeye", blackDiamond,
+				"Served with grilled asparagus and wild rice.",
+				"n/a", "$21.00");
+		MenuItem porterhouse = new MenuItem("22 oz Porterhouse", blackDiamond,
+				"Served with candied bacon brussel sprouts and a baked sweet potato.",
+				"n/a", "$21.00");
+		MenuItem ovenRoastedChickenBreast = new MenuItem("Oven Roasted Chicken Breast", blackDiamond,
+				"Fresh, juicy, oven roasted chicken breast perfectly seasoned with herbs. Comes with roasted corn and garlic mashed red potatoes.",
+				"n/a", "$14.00");
 
 		menuItemRepo.save(baconCheeseFries);
+		menuItemRepo.save(allAmericanBurger);
+		menuItemRepo.save(angusPrimeRibeye);
+		menuItemRepo.save(porterhouse);
+		menuItemRepo.save(ovenRoastedChickenBreast);
+		
+		MenuItem steamedDumplings = new MenuItem("Steamed Dumplings", luckyDragon,
+				"A healthy portion of delicious dumplings filled with pork.",
+				"n/a", "$6.25");
+		MenuItem chickenWithCashewNut = new MenuItem("Chicken with Cashew Nut", luckyDragon,
+				"A perfect ratio of chicken to cashews in a brown sauce over a bed or steamed or fried rice.",
+				"n/a", "$9.95");
+		MenuItem shrimpLoMein = new MenuItem("Shrimp Lo Mein", luckyDragon,
+				"Lo Mein noodles with shrimp in a light sauce.",
+				"n/a", "$9.95");
+		MenuItem happyFamily = new MenuItem("Happy Family", luckyDragon,
+				"Shrimp, chicken, beef and pork stir-fried with vegetables in delicious brown sauce.",
+				"n/a", "$13.25");
+		MenuItem maiTai = new MenuItem("Mai Tai", luckyDragon,
+				"Rum, coconut rum, grenadine, pineapple juice, orange juice. Delicious!",
+				"n/a", "$4.95");
+		
+		menuItemRepo.save(steamedDumplings);
+		menuItemRepo.save(chickenWithCashewNut);
+		menuItemRepo.save(shrimpLoMein);
+		menuItemRepo.save(happyFamily);
+		menuItemRepo.save(maiTai);
+		
+		MenuItem shortRibRisotto = new MenuItem("Short Rib Risotto", coopersHawk,
+				"Carnaroli Rice, Mushroom, Sweet Onion, White Wine, Shaved Parmesan, White Truffle Oil.",
+				"3.5", "$24.99");
+		MenuItem jambalaya = new MenuItem("Jambalaya", coopersHawk,
+				"Chicken, Shrimp, Andouille, Onion, Tomato, Peppers, Jasmine Rice.",
+				"4.0", "$18.99");
+		MenuItem coopersHawkCalamari = new MenuItem("Cooper's Hawk Calamari", coopersHawk,
+				"Sweet Chili-Ponzu Glaze, Cilantro, Sesame-Sriracha Aioli.",
+				"4.2", "$12.99");
+		MenuItem romanceRed = new MenuItem("83 Romance Red", coopersHawk,
+				"Concord grapes with hints of maraschino cherry and spice.",
+				"4.7", "$12.99");
+		
+		menuItemRepo.save(shortRibRisotto);
+		menuItemRepo.save(jambalaya);
+		menuItemRepo.save(coopersHawkCalamari);
+		menuItemRepo.save(romanceRed);
 	}
 
 }

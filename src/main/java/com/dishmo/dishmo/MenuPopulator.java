138fad2fea5163c630712f7e2866eab5176cd955
Cindy Colucy <ckcolucy@gmail.com>
2017-07-24 16:33:23 -0400

    made about link work

--- src/main/java/com/dishmo/dishmo/MenuPopulator.java
+++ src/main/java/com/dishmo/dishmo/MenuPopulator.java
@@ -32,61 +32,77 @@ public class MenuPopulator implements CommandLineRunner {
 		breakfastRepo.save(hashbrowns);
 
 		// Lunch
-//				SampleMenu lunch = new SampleMenu();
-//				sampleMenuRepo.save(lunch);
-//
-//				Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your preference", "5", breakfast, "");
-//				Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy bacon", "3", breakfast, "");
-//				Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes", "5", breakfast, "");
-//				Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried hashbrown patties", "3", breakfast, "");
-//
-//				breakfastRepo.save(eggs);
-//				breakfastRepo.save(bacon);
-//				breakfastRepo.save(pancakes);
-//				breakfastRepo.save(hashbrowns);
-				
+		// SampleMenu lunch = new SampleMenu();
+		// sampleMenuRepo.save(lunch);
+		//
+		// Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your
+		// preference", "5", breakfast, "");
+		// Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy
+		// bacon", "3", breakfast, "");
+		// Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes",
+		// "5", breakfast, "");
+		// Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried
+		// hashbrown patties", "3", breakfast, "");
+		//
+		// breakfastRepo.save(eggs);
+		// breakfastRepo.save(bacon);
+		// breakfastRepo.save(pancakes);
+		// breakfastRepo.save(hashbrowns);
+
 		// Dinner
-//				SampleMenu breakfast = new SampleMenu();
-//				sampleMenuRepo.save(breakfast);
-//
-//				Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your preference", "5", breakfast, "");
-//				Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy bacon", "3", breakfast, "");
-//				Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes", "5", breakfast, "");
-//				Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried hashbrown patties", "3", breakfast, "");
-//
-//				breakfastRepo.save(eggs);
-//				breakfastRepo.save(bacon);
-//				breakfastRepo.save(pancakes);
-//				breakfastRepo.save(hashbrowns);
-				
+		// SampleMenu breakfast = new SampleMenu();
+		// sampleMenuRepo.save(breakfast);
+		//
+		// Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your
+		// preference", "5", breakfast, "");
+		// Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy
+		// bacon", "3", breakfast, "");
+		// Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes",
+		// "5", breakfast, "");
+		// Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried
+		// hashbrown patties", "3", breakfast, "");
+		//
+		// breakfastRepo.save(eggs);
+		// breakfastRepo.save(bacon);
+		// breakfastRepo.save(pancakes);
+		// breakfastRepo.save(hashbrowns);
+
 		// Appetizer
-//				SampleMenu breakfast = new SampleMenu();
-//				sampleMenuRepo.save(breakfast);
-//
-//				Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your preference", "5", breakfast, "");
-//				Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy bacon", "3", breakfast, "");
-//				Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes", "5", breakfast, "");
-//				Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried hashbrown patties", "3", breakfast, "");
-//
-//				breakfastRepo.save(eggs);
-//				breakfastRepo.save(bacon);
-//				breakfastRepo.save(pancakes);
-//				breakfastRepo.save(hashbrowns);
+		// SampleMenu breakfast = new SampleMenu();
+		// sampleMenuRepo.save(breakfast);
+		//
+		// Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your
+		// preference", "5", breakfast, "");
+		// Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy
+		// bacon", "3", breakfast, "");
+		// Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes",
+		// "5", breakfast, "");
+		// Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried
+		// hashbrown patties", "3", breakfast, "");
+		//
+		// breakfastRepo.save(eggs);
+		// breakfastRepo.save(bacon);
+		// breakfastRepo.save(pancakes);
+		// breakfastRepo.save(hashbrowns);
 
 		// Dessert
-//				SampleMenu breakfast = new SampleMenu();
-//				sampleMenuRepo.save(breakfast);
-//
-//				Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your preference", "5", breakfast, "");
-//				Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy bacon", "3", breakfast, "");
-//				Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes", "5", breakfast, "");
-//				Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried hashbrown patties", "3", breakfast, "");
-//
-//				breakfastRepo.save(eggs);
-//				breakfastRepo.save(bacon);
-//				breakfastRepo.save(pancakes);
-//				breakfastRepo.save(hashbrowns);
-//
+		// SampleMenu breakfast = new SampleMenu();
+		// sampleMenuRepo.save(breakfast);
+		//
+		// Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your
+		// preference", "5", breakfast, "");
+		// Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy
+		// bacon", "3", breakfast, "");
+		// Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes",
+		// "5", breakfast, "");
+		// Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried
+		// hashbrown patties", "3", breakfast, "");
+		//
+		// breakfastRepo.save(eggs);
+		// breakfastRepo.save(bacon);
+		// breakfastRepo.save(pancakes);
+		// breakfastRepo.save(hashbrowns);
+		//
 
 	}
 

624e49ae09221ed5812a625a573fe07a7af95cf0
Cindy Colucy <ckcolucy@gmail.com>
2017-07-25 15:07:08 -0400

    fixed goof

--- src/main/java/com/dishmo/dishmo/MenuPopulator.java
+++ src/main/java/com/dishmo/dishmo/MenuPopulator.java
@@ -19,13 +19,13 @@ public class MenuPopulator implements CommandLineRunner {
 
 	@Resource
 	private DessertRepository dessertRepo;
-	
+
 	@Resource
 	private DrinkRepository drinkRepo;
 
 	@Resource
 	private AlcoholicBeverageRepository alcBevRepo;
-	
+
 	@Resource
 	private AppetizerRepository appetizerRepo;
 
@@ -139,7 +139,6 @@ public class MenuPopulator implements CommandLineRunner {
 		lunchRepo.save(highStreetVegetable);
 
 		// Dinner
-<<<<<<< HEAD
 		SampleMenu dinner = new SampleMenu();
 		sampleMenuRepo.save(dinner);
 
@@ -226,7 +225,7 @@ public class MenuPopulator implements CommandLineRunner {
 		alcBevRepo.save(buehlerChardonnay);
 		alcBevRepo.save(smithAndHookCabernet);
 
-		//Drinks
+		// Drinks
 		SampleMenu drink = new SampleMenu();
 		sampleMenuRepo.save(drink);
 
@@ -257,125 +256,28 @@ public class MenuPopulator implements CommandLineRunner {
 		drinkRepo.save(hotTea);
 
 		// Dessert
-		// SampleMenu breakfast = new SampleMenu();
-		// sampleMenuRepo.save(breakfast);
-		//
-		// Breakfast eggs = new Breakfast("Egg Breakfast", "2 eggs cooked to your
-		// preference", "5", breakfast, "");
-		// Breakfast bacon = new Breakfast("Bacon Breakfast", "3 slices of crispy
-		// bacon", "3", breakfast, "");
-		// Breakfast pancakes = new Breakfast("Pancake Breakfast", "3 fluffy pancakes",
-		// "5", breakfast, "");
-		// Breakfast hashbrowns = new Breakfast("Hashbrowns Breakfast", "2 fried
-		// hashbrown patties", "3", breakfast, "");
-		//
-		// breakfastRepo.save(eggs);
-		// breakfastRepo.save(bacon);
-		// breakfastRepo.save(pancakes);
-		// breakfastRepo.save(hashbrowns);
-		//
-=======
-		 SampleMenu dinner = new SampleMenu();
-		 sampleMenuRepo.save(dinner);
-		
-		 Dinner simpleSaladD = new Dinner("Simple Salad", "Crisp greens and rosemary croutons with cafe vinaigrette.", "5", dinner, "/images/Dinner/simple-salad.jpeg");
-		 Dinner choppedSaladD = new Dinner("Chopped Salad", "Applewood smoked turkey, bacon, avocado, blue cheese, almonds, croutons and apples with face vinaigrette.", "15", dinner, "/images/Dinner/chopped-salad.jpeg");
-		 Dinner beechwoldSaladD = new Dinner("Beechwold Salad", "Roasted chicken, dates, avocado, tomatoes, corn, goat cheese, croutons and almonds with champagne vinaigrette.", "14", dinner, "/images/Dinner/beechwald-salad.jpeg");
-		 Dinner citrusCrunchSaladD = new Dinner("Citrus Crunch Salad", "Shredded chicken, crisp vegetables, mango and fresh herbs with tortillas and peanut lime dressing.", "14.5", dinner, "/images/Dinner/citrus-crunch-salad.jpg");
-		 Dinner buddhaBowlD = new Dinner("Buddha Bowl", "Pan seared organic tofu or roasted chicken, long-grain brown rice with peanut sauce and bright veggies.", "13", dinner, "/images/Dinner/buddha-bowl.jpeg");
-		 Dinner squareMealD = new Dinner("Square Meal", "Spicy organic black beans over brown rice with sauteed peppers & onions, avocado, cheddar and corn tortillas.", "10", dinner, "/images/Dinner/square-meal.jpg");
-		 Dinner northstarBurgerD = new Dinner("Northstar Burger", "Just made with organic black beans, brown rice and beets, topped with white cheddar, kale, tomato, pickle and onion + simple salad.", "14", dinner, "/images/Dinner/northstar-burger.jpeg");
-		 Dinner classicCheeseburgerD = new Dinner("Classic Cheeseburger", "Nilman Ranch brisket and chuck*, ground in-house daily, topped with cheddar, lettuce, tomato, onion, pickle + breakfast potatoes.", "15", dinner, "/Dinner/Lunch/classic-cheeseburger.jpeg");
-		 Dinner fishSandwichD = new Dinner("Fish Sandwich", "Grilled barramundi filet with lettuce, tomato, red onion, and herb aioli + simple salad.", "17", dinner, "/images/Dinner/fish-sandwich.jpg");
-		 Dinner chickenAndAvoSandwichD = new Dinner("Chicken & Avocado Sandwich", "Roasted Bell & Evans chicken, avocado, smoked gouda, roasted red peppers and greens with cayenne aioli + sweet potato fries.", "15", dinner, "/images/Dinner/chicken-and-avocado-sandwich.jpg");
-		 Dinner thaiburritoD = new Dinner("Thai Burrito", "Seared organic tofu or roasted chicken with peanut sauce, crunchy slaw and brown rice + tortilla chips & Thai dipping sauce.", "12.5", dinner, "/images/Dinner/thai-burrito.jpg");
-		 Dinner sweetBasilBurritoD = new Dinner("Sweet Basil Burrito", "Rotisserie roasted chicken or seared tofu with creamy pesto, sauteed veggies and brown rice + tortilla chips & Northstar's salsa", "13", dinner, "/images/Dinner/sweet-basil-burrito.jpg");
-		 Dinner spicyHarvestD = new Dinner("Spicy Harvest", "Smoked gouda, pepperoni, corn, jalapenos with organic tomato sauce on a whole wheat crust.", "14", dinner, "/images/Dinner/spicy-harvest-pizza.jpg");
-		 Dinner housemadeSausageD = new Dinner("Housemade Sausage", "Crumbled sausace and Mamma Lil's Peppers paired with organic tomato sauce, provolone and parmesan.", "14", dinner, "/images/Dinner/housemade-sausage-pizza.jpg");
-		 Dinner smokedGoudaAndChickenD = new Dinner("Smoked Gouda + Chicken", "Rotisserie roasted chicken, Northstar's BBQ sauce, red peppers, onions, cilantro and peanuts.", "14", dinner, "/images/Dinner/smoked-gouda-and-chicken-pizza.jpeg");
-		 Dinner highStreetVegetableD = new Dinner("High Street Vegetable", "Crunchy veggies, tomatoes, mushrooms, jalapeños, sunflower seeds, aged provolone and Grana Padano.", "12", dinner, "/images/Dinner/high-street-vegetable-pizza.jpeg");
-		 
-		 dinnerRepo.save(simpleSaladD);
-		 dinnerRepo.save(choppedSaladD);
-		 dinnerRepo.save(beechwoldSaladD);
-		 dinnerRepo.save(citrusCrunchSaladD);
-		 dinnerRepo.save(buddhaBowlD);
-		 dinnerRepo.save(squareMealD);
-		 dinnerRepo.save(northstarBurgerD);
-		 dinnerRepo.save(classicCheeseburgerD);
-		 dinnerRepo.save(fishSandwichD);
-		 dinnerRepo.save(chickenAndAvoSandwichD);
-		 dinnerRepo.save(thaiburritoD);
-		 dinnerRepo.save(sweetBasilBurritoD);
-		 dinnerRepo.save(spicyHarvestD);
-		 dinnerRepo.save(housemadeSausageD);
-		 dinnerRepo.save(smokedGoudaAndChickenD);
-		 dinnerRepo.save(highStreetVegetableD);
-		 
-//		 Alcoholic Beverages
-		 SampleMenu alcoholicBeverage = new SampleMenu();
-		 sampleMenuRepo.save(alcoholicBeverage);
-		 
-//		 AlcoholicBeverage something = new AlcoholicBeverage("name", "whereFrom", "price", drink, "/images/Drink/");
-		 AlcoholicBeverage rhinegeistCougarBlondeAle = new AlcoholicBeverage("Rhinegeist Cougar Blonde Ale", "Ohio", "5", alcoholicBeverage, "/images/Drink/rhinegeist-cougar-blonde-ale.jpeg");
-		 AlcoholicBeverage northHighMilkStout = new AlcoholicBeverage("North High Milk Stout", "Ohio", "5", alcoholicBeverage, "/images/Drink/north-high-milk-stout.jpg");
-		 AlcoholicBeverage buehlerChardonnay = new AlcoholicBeverage("Buehler Chardonnay", "Napa", "9", alcoholicBeverage, "/images/Drink/buehler-chardonnay.jpeg");
-		 AlcoholicBeverage smithAndHookCabernet = new AlcoholicBeverage("Smith & Hook Cabernet", "Monterey", "10", alcoholicBeverage, "/images/Drink/smith-and-hook-cabernet.jpg");
-		 		 
-		 alcBevRepo.save(rhinegeistCougarBlondeAle);
-		 alcBevRepo.save(northHighMilkStout);
-		 alcBevRepo.save(buehlerChardonnay);
-		 alcBevRepo.save(smithAndHookCabernet);
-		
-//		 Drinks
-		 SampleMenu drink = new SampleMenu();
-		 sampleMenuRepo.save(drink);
-		
-//		 Drink something = new Drink("name", "price", drink, "/images/Drink/");
-		 Drink shootingStar = new Drink("Shooting Star", "5", drink, "/images/Drink/shooting-star.jpeg");
-		 Drink orangeJuice = new Drink("Orange Juice", "4.5", drink, "/images/Drink/orange-juice.jpeg");
-		 Drink organicCarrotJuice = new Drink("Organic Carrot Juice", "5", drink, "/images/Drink/organic-carrot-juice.jpeg");
-		 Drink strawberrySmoothie = new Drink("Strawberry Smoothie", "6", drink, "/images/Drink/strawberry-smoothie.jpeg");
-		 Drink blueberrySmoothie = new Drink("Blueberry Smoothie", "6", drink, "/images/Drink/blueberry-smoothie.jpeg");
-		 Drink gingerAle = new Drink("Ginger Ale", "4", drink, "/images/Drink/gingerale.jpg");
-		 Drink coldBrewedIcedCoffee = new Drink("Cold-Brewed Iced Coffee", "3.5", drink, "/images/Drink/cold-brewed-iced-coffee.jpeg");
-		 Drink mintIcedCoffee = new Drink("Mint Iced Coffee", "4.5", drink, "/images/Drink/mint-iced-tea.jpeg");
-		 Drink singleOriginCoffee = new Drink("Single Origin Coffee", "3", drink, "/images/Drink/single-origin-coffee.jpeg");
-		 Drink hotTea = new Drink("Hot Tea", "3", drink, "/images/Drink/hot-tea.jpeg");
-		 
-		 drinkRepo.save(shootingStar);
-		 drinkRepo.save(orangeJuice);
-		 drinkRepo.save(organicCarrotJuice);
-		 drinkRepo.save(strawberrySmoothie);
-		 drinkRepo.save(blueberrySmoothie);
-		 drinkRepo.save(gingerAle);
-		 drinkRepo.save(coldBrewedIcedCoffee);
-		 drinkRepo.save(mintIcedCoffee);
-		 drinkRepo.save(singleOriginCoffee);
-		 drinkRepo.save(hotTea);
-		 
-//		 Dessert
-		 SampleMenu dessert = new SampleMenu();
-		 sampleMenuRepo.save(dessert);
-		
-//		 Dessert something = new Dessert("name", "price", dessert, "/images/Dessert/");
-		 Dessert peanutButterCookie = new Dessert("Peanut Butter Cookie", "3", dessert, "/images/Dessert/peanut-butter-cookie.jpeg");
-		 Dessert darkChocolateTruffleCookie = new Dessert("Dark Chocolate Truffle Cookie", "3", dessert, "/images/Dessert/dark-chocolate-truffle-cookie.jpg");
-		
-		 dessertRepo.save(peanutButterCookie);
-		 dessertRepo.save(darkChocolateTruffleCookie);
->>>>>>> ce72c504ecbc04bae0dd3334bfb019d92f4ddb08
+		SampleMenu dessert = new SampleMenu();
+		sampleMenuRepo.save(dessert);
 
-//		 Appetizer
-		 SampleMenu appetizer = new SampleMenu();
-		 sampleMenuRepo.save(appetizer);
-		
-//		 Appetizer something = new Appetizer("name", "price", dessert, "/images/Dessert/");
-		 Appetizer pimentoCheeseDip = new Appetizer("Pimento Cheese Dip", "7", appetizer, "/images/Dessert/pimento-cheese-dip.jpeg");
-		 Appetizer madeToOrderGuacamole = new Appetizer("Made to Order Guacamole", "7", appetizer, "/images/Dessert/made-to-order-guacamole.jpg");
-		
-		 appetizerRepo.save(pimentoCheeseDip);
-		 appetizerRepo.save(madeToOrderGuacamole);
+		Dessert peanutButterCookie = new Dessert("Peanut Butter Cookie", "3", dessert,
+				"/images/Dessert/peanut-butter-cookie.jpeg");
+		Dessert darkChocolateTruffleCookie = new Dessert("Dark Chocolate Truffle Cookie", "3", dessert,
+				"/images/Dessert/dark-chocolate-truffle-cookie.jpg");
+
+		dessertRepo.save(peanutButterCookie);
+		dessertRepo.save(darkChocolateTruffleCookie);
+
+		// Appetizer
+		SampleMenu appetizer = new SampleMenu();
+		sampleMenuRepo.save(appetizer);
+
+		Appetizer pimentoCheeseDip = new Appetizer("Pimento Cheese Dip", "7", appetizer,
+				"/images/Dessert/pimento-cheese-dip.jpeg");
+		Appetizer madeToOrderGuacamole = new Appetizer("Made to Order Guacamole", "7", appetizer,
+				"/images/Dessert/made-to-order-guacamole.jpg");
+
+		appetizerRepo.save(pimentoCheeseDip);
+		appetizerRepo.save(madeToOrderGuacamole);
 	}
 
 }

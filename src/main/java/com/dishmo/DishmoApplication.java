040b2a61d469336c399ec28f3703898707027e5d
Michaelondon <michaelondon6@gmail.com>
2017-07-27 14:54:14 -0400

    Michael Menu
    my menu with image

---
+++ src/main/java/com/dishmo/DishmoApplication.java
@@ -0,0 +1,12 @@
+package com.dishmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishmoApplication.class, args);
+	}
+}

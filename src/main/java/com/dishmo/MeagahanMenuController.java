84dc37d0ff12f88dccc93032fad02454dee6f6f8
Meaghan <meaghan.rea@gmail.com>
2017-07-27 12:30:15 -0400

    added meaghans menu and updated nav logo

---
+++ src/main/java/com/dishmo/MeagahanMenuController.java
@@ -0,0 +1,68 @@
+package com.dishmo;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MeagahanMenuController {
+
+		@Resource
+		private DrinkRepository drinkRepo;
+		
+		@Resource
+		private AppetizerRepository appetizerRepo;
+		
+		@Resource
+		private BreakfastRepository breakfastRepo;
+		
+		@Resource
+		private LunchRepository lunchRepo;
+		
+		@Resource
+		private DinnerRepository dinnerRepo;
+		
+		@Resource
+		private DessertRepository dessertRepo;
+		
+		@Resource
+		private AlcoholicBeverageRepository alcoholicBeverageRepo;
+		
+		@RequestMapping("/meaghan-menu")
+		public String fetchAdamMenu(Model model) {
+			return "/meaghan-menu-categories";
+		}
+//		@RequestMapping("/adam-menu-drinks")
+//		public String fetchAdamMenuDrinks(Model model) {
+//			model.addAttribute("drinks", drinkRepo.findAll());
+//			model.addAttribute("alcoholicBeverages", alcoholicBeverageRepo.findAll());
+//			return "/adam-menu-categories/adam-menu-drinks";
+//		}
+//		@RequestMapping("/adam-menu-appetizers")
+//		public String fetchAdamMenuApps(Model model) {
+//			model.addAttribute("appetizers", appetizerRepo.findAll());
+//			return "/adam-menu-categories/adam-menu-appetizers";
+//		}
+		@RequestMapping("/meaghan-menu-breakfasts")
+		public String fetchAdamMenuBreakfasts(Model model) {
+			model.addAttribute("breakfasts", breakfastRepo.findAll());
+			return "/meaghan-menu-categories/meaghan-menu-breakfasts";
+		}
+//		@RequestMapping("/adam-menu-lunches")
+//		public String fetchAdamMenuLunches(Model model) {
+//			model.addAttribute("lunches", lunchRepo.findAll());
+//			return "/adam-menu-categories/adam-menu-lunches";
+//		}
+//		@RequestMapping("/adam-menu-dinners")
+//		public String fetchAdamMenuDinners(Model model) {
+//			model.addAttribute("dinners", dinnerRepo.findAll());
+//			return "/adam-menu-categories/adam-menu-dinners";
+//		}
+//		@RequestMapping("/adam-menu-desserts")
+//		public String fetchAdamMenuDesserts(Model model) {
+//			model.addAttribute("desserts", dessertRepo.findAll());
+//			return "/adam-menu-categories/adam-menu-desserts";
+//		}
+}

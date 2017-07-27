de67388f7cb12191aaeae47f2a1f3eaf3381208a
Cindy Colucy <ckcolucy@gmail.com>
2017-07-27 15:16:16 -0400

    style2

--- src/main/java/com/dishmo/CindyController.java
+++ src/main/java/com/dishmo/CindyController.java
@@ -34,4 +34,41 @@ public class CindyController {
 	public String fetchCindyMenu(Model model) {
 		return "/cindy-html/cindy-index";
 	}
+
+	@RequestMapping("/cindy-menu-drinks")
+	public String fetchCindyMenuDrinks(Model model) {
+		model.addAttribute("drinks", drinkRepo.findAll());
+		model.addAttribute("alcoholicBeverages", alcoholicBeverageRepo.findAll());
+		return "/cindy-html/drinks-index";
+	}
+
+	@RequestMapping("/cindy-menu-appetizers")
+	public String fetchCindyMenuApps(Model model) {
+		model.addAttribute("appetizers", appetizerRepo.findAll());
+		return "/cindy-html/appetizer-index";
+	}
+
+	@RequestMapping("/cindy-menu-breakfasts")
+	public String fetchCindyMenuBreakfasts(Model model) {
+		model.addAttribute("breakfasts", breakfastRepo.findAll());
+		return "/cindy-html/breakfast-index";
+	}
+
+	@RequestMapping("/cindy-menu-lunches")
+	public String fetchCindyMenuLunches(Model model) {
+		model.addAttribute("lunches", lunchRepo.findAll());
+		return "/cindy-html/lunch-index";
+	}
+
+	@RequestMapping("/cindy-menu-dinners")
+	public String fetchCindyMenuDinners(Model model) {
+		model.addAttribute("dinners", dinnerRepo.findAll());
+		return "/cindy-html/dinner-index";
+	}
+
+	@RequestMapping("/cindy-menu-desserts")
+	public String fetchCindyMenuDesserts(Model model) {
+		model.addAttribute("desserts", dessertRepo.findAll());
+		return "/cindy-html/dessert-index";
+	}
 }

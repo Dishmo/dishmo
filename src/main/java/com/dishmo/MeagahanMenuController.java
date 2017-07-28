aff37d8f885d555419cecb22c6ae99314083c7b1
Meaghan <meaghan.rea@gmail.com>
2017-07-28 14:13:52 -0400

    making changes to meaghans menu

--- src/main/java/com/dishmo/MeagahanMenuController.java
+++ src/main/java/com/dishmo/MeagahanMenuController.java
@@ -31,38 +31,15 @@ public class MeagahanMenuController {
 		private AlcoholicBeverageRepository alcoholicBeverageRepo;
 		
 		@RequestMapping("/meaghan-menu")
-		public String fetchAdamMenu(Model model) {
-			return "/meaghan-menu-categories";
-		}
-		@RequestMapping("/meaghan-menu-drinks")
-		public String fetchAdamMenuDrinks(Model model) {
+		public String fetchMeaghanMenu(Model model) {
 			model.addAttribute("drinks", drinkRepo.findAll());
 			model.addAttribute("alcoholicBeverages", alcoholicBeverageRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-drinks";
-		}
-		@RequestMapping("/meaghan-menu-appetizers")
-		public String fetchAdamMenuApps(Model model) {
 			model.addAttribute("appetizers", appetizerRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-appetizers";
-		}
-		@RequestMapping("/meaghan-menu-breakfasts")
-		public String fetchAdamMenuBreakfasts(Model model) {
 			model.addAttribute("breakfasts", breakfastRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-breakfasts";
-		}
-		@RequestMapping("/meaghan-menu-lunches")
-		public String fetchAdamMenuLunches(Model model) {
 			model.addAttribute("lunches", lunchRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-lunches";
-		}
-		@RequestMapping("/meaghan-menu-dinners")
-		public String fetchAdamMenuDinners(Model model) {
 			model.addAttribute("dinners", dinnerRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-dinners";
-		}
-		@RequestMapping("/meaghan-menu-desserts")
-		public String fetchAdamMenuDesserts(Model model) {
 			model.addAttribute("desserts", dessertRepo.findAll());
-			return "/meaghan-menu-categories/meaghan-menu-desserts";
+			return "/meaghan-menu-categories/meaghan-menu";
 		}
+	
 }

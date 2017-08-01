4124578eb4e331ad07920018eb69e7920bf76d6d
Adam <adamyoungpeter@gmail.com>
2017-08-01 11:35:12 -0400

    fixed breakfast type

--- src/main/java/com/dishmo/MenuPopulator.java
+++ src/main/java/com/dishmo/MenuPopulator.java
@@ -41,7 +41,7 @@ public class MenuPopulator implements CommandLineRunner {
 
 		Breakfast housemadeGranola = new Breakfast("Housemade Granola",
 				"Organic rolled oats, almonds, pecans, fat-free Greek yogurt, fresh fruit, honey and pistachio praline brittle.",
-				"9", breakfast, "/images/Breakfast/housemade-granola.jpeg", "/images/Breakfast/housemade-granola-2.jpg", "/images/Breakfast/housemade-granola.jpg-3");
+				"9", breakfast, "/images/Breakfast/housemade-granola.jpeg", "/images/Breakfast/housemade-granola-2.jpg", "/images/Breakfast/housemade-granola-3.jpg");
 		Breakfast prosciuttoAndPoachedEggs = new Breakfast("Prosciutto and Poached Eggs",
 				"Three organic valley eggs* and La Quercia prosciutto on toasted rosemary focaccia with parmesan.",
 				"13", breakfast, "/images/Breakfast/prosciutto-and-poached-eggs.jpeg", "/images/Breakfast/prosciutto-and-poached-eggs-2.jpg", "/images/Breakfast/prosciutto-and-poached-eggs-3.jpg");
